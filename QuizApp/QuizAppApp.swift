c6e5d5c111f3c20d070503df1b54e5c842c91047
Mohammad Azam <azamsharp@gmail.com>
2021-10-21 13:40:13 -0500

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Mohammad Azam on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
